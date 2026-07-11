c8995333c7300bd767d2f41453a499287a4e8b24
harshfoundercode <Harsh@foundercodes.com>
2026-07-11 12:20:11 +0530

    all done

--- src/pages/config/ApiEndPoints.js
+++ src/pages/config/ApiEndPoints.js
@@ -36,7 +36,6 @@ const API_ENDPOINTS = {
     filter: 'transactions/filter',
     retry: 'transactions/retry',
     return: 'transactions/return',
-    resendWebHook:'merchant/resend-webhook'
   },
 
   reports: {
